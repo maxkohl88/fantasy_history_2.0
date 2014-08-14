140cf0735d1853a31b4663c8c4fd8b909b358502
Max Kohl <maxkohl810@gmail.com>
2014-08-14 17:17:12 -0400

    move importio import method into the Record model

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,13 +1,6 @@
 require 'importio.rb'
 
 class RecordsController < ApplicationController
-  #constant to save the sport abbreviations needed to construct the league page
-  #url for ESPN
-  SPORTS_ABBREVIATIONS = {
-    baseball: 'flb',
-    basketball: 'fba',
-    football: 'ffl'
-  }
 
   def new
     @sport = Sport.find(params[:sport_id])
@@ -17,75 +10,11 @@ class RecordsController < ApplicationController
   end
 
   def create
-
     @sport = Sport.find(params[:sport_id])
     @league = @sport.leagues.find(params[:league_id])
-    @new_records = []
 
-    #defines the Importio API client
-    client = Importio::new("b66ea9de-3b8e-497f-931a-91d25710d3b1", ENV["IMPORTIO_TOKEN"], "https://query.import.io")
-    #create a blank array where the returned data will be stored
-    data_rows = []
-
-    #define callbacks that are sent with the API query
-    callback = lambda do |query, message|
-      if message["type"] == "DISCONNECT"
-        puts "The query was cancelled as the client was disconnected"
-      end
-      if message["type"] == "MESSAGE"
-        if message["data"].key?("errorType")
-          puts "Got an error!"
-          puts JSON.pretty_generate(message["data"])
-        else
-          puts "Got data!"
-          puts JSON.pretty_generate(message["data"])
-          data_rows << message["data"]["results"]
-        end
-      end
-      if query.finished
-        puts "Query finished"
-      end
-    end
-
-    #connect to the API
-    client.connect
-
-    #dynamically build the request URL and make the request to the Importio API
-    client.query({"input"=>{"webpage/url"=>"http://games.espn.go.com/#{SPORTS_ABBREVIATIONS[@sport.name.downcase.to_sym]}/standings?leagueId=#{@league.espn_id}&seasonId=#{record_params[:year]}"},"connectorGuids"=>["d5170dfc-fdd8-4227-8d19-446480420690"]}, callback )
-
-    #join all data together
-    client.join
-    #disconnect from API
-    client.disconnect
-    #extract the actual JSON hash from the data_results
-    importio_data_array = data_rows[0]
-
-    #this is a beast. loop through each returned block of JSON and create new
-    #teams and new records for that league. Teams validate on uniqueness so
-    #this won't create a new team if it already exists in the database.
-    importio_data_array.each do |row|
-      @team = @league.teams.new
-      @team.name = row['team/_title']
-      @team.team_url = row['team/_source'].split('&seasonId')[0]
-
-      @team.save
-
-      @record = @league.records.new(record_params)
-      @record.wins = row['wins/_source']
-      @record.losses = row['losses/_source']
-      @record.ties = row['ties/_source']
-      @record.year = record_params[:year]
-      @record.team_id = Team.find_by(team_url: row['team/_source'].split('&seasonId')[0]).id
-
-      @new_records << @record
-    end
-
-    #save all those new records
-    if @new_records.each { |record| record.save }
-      redirect_to sport_league_path(@sport, @league), success: 'League records imported!'
-    else
-      render :new
-    end
+    Record.import_from_importio(@sport, @league, record_params)
+    redirect_to sport_league_path(@sport, @league)
   end
 
   private
--- app/models/record.rb
+++ app/models/record.rb
@@ -6,32 +6,35 @@ class Record < ActiveRecord::Base
 
   validates :wins, :losses, :ties, :year, :league_id, :team_id, presence: true
 
-  def importio_import
-    client = Importio::new("b66ea9de-3b8e-497f-931a-91d25710d3b1","v/S9sPDS/kPHTbxtzA8F9l6SKg4x1PDkWeNXDOxYPBbIgWlawN5xrodjtnds6mgQCS6g8AFS9AKPoTaNM9kI1Q==", "https://query.import.io")
-    # Once we have started the client and authenticated, we need to connect it to the server:
+  #constant to save the sport abbreviations needed to construct the league page
+  #url for ESPN
+  SPORTS_ABBREVIATIONS = {
+    baseball: 'flb',
+    basketball: 'fba',
+    football: 'ffl'
+  }
 
-    # Define here a global variable that we can put all our results in to when they come back from
-    # the server, so we can use the data later on in the script
+  def self.import_from_importio(current_sport, current_league, record_params)
+
+    new_records = []
+
+    #defines the Importio API client
+    client = Importio::new("b66ea9de-3b8e-497f-931a-91d25710d3b1", ENV["IMPORTIO_TOKEN"], "https://query.import.io")
+    #create a blank array where the returned data will be stored
     data_rows = []
 
-    # In order to receive the data from the queries we issue, we need to define a callback method
-    # This method will receive each message that comes back from the queries, and we can take that
-    # data and store it for use in our app
+    #define callbacks that are sent with the API query
     callback = lambda do |query, message|
-      # Disconnect messages happen if we disconnect the client library while a query is in progress
       if message["type"] == "DISCONNECT"
         puts "The query was cancelled as the client was disconnected"
       end
       if message["type"] == "MESSAGE"
         if message["data"].key?("errorType")
-          # In this case, we received a message, but it was an error from the external service
           puts "Got an error!"
           puts JSON.pretty_generate(message["data"])
         else
-          # We got a message and it was not an error, so we can process the data
           puts "Got data!"
           puts JSON.pretty_generate(message["data"])
-          # Save the data we got in our dataRows variable for later
           data_rows << message["data"]["results"]
         end
       end
@@ -39,15 +42,49 @@ class Record < ActiveRecord::Base
         puts "Query finished"
       end
     end
-    client.connect
-    # Issue queries to your data sources with your specified inputs
-    # You can modify the inputs and connectorGuids so as to query your own sources
-    client.query({"input"=>{"webpage/url"=>"http://games.espn.go.com/flb/standings?leagueId=140424&seasonId=2014"},"connectorGuids"=>["d5170dfc-fdd8-4227-8d19-446480420690"]}, callback )
 
-    # Now we have issued all of the queries, we can wait for all of the threads to complete meaning the queries are done
+    #connect to the API
+    client.connect
+
+    #dynamically build the request URL and make the request to the Importio API
+    client.query({"input"=>{"webpage/url"=>"http://games.espn.go.com/#{SPORTS_ABBREVIATIONS[current_sport.name.downcase.to_sym]}/standings?leagueId=#{current_league.espn_id}&seasonId=#{record_params[:year]}"},"connectorGuids"=>["d5170dfc-fdd8-4227-8d19-446480420690"]}, callback )
+
+    #join all data together
     client.join
+    #disconnect from API
     client.disconnect
-    results = JSON.pretty_generate(data_rows)
+    #extract the actual JSON hash from the data_results
+    importio_data_array = data_rows[0]
+
+    #this is a beast. loop through each returned block of JSON and create new
+    #teams and new records for that league. Teams validate on uniqueness so
+    #this won't create a new team if it already exists in the database.
+    importio_data_array.each do |row|
+      team = current_league.teams.new
+      team.name = row['team/_title']
+      team.team_url = row['team/_source'].split('&seasonId')[0]
+
+      team.save
+
+      record = current_league.records.new(record_params)
+      record.wins = row['wins/_source']
+      record.losses = row['losses/_source']
+      record.ties = row['ties/_source']
+      record.year = record_params[:year]
+      record.team_id = Team.find_by(team_url: row['team/_source'].split('&seasonId')[0]).id
+
+      record.save!
+    end
+
+    # wrap all database modifying code in a transaction, use the bang form of 
+    # the save method
+    #save all those new records
+    # if new_records.each { |record| record.save }
+    #   redirect_to sport_league_path(current_sport, current_league), success: 'League records imported!'
+    # else
+    #   render :new
+    # end
+    
   end
 end
 
